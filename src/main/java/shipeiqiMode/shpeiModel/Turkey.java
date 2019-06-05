a97c31eec2518d9d16f8ef1d74d20cfbc3d4d31c
sky5cai <1147861255@qq.com>
2019-06-05 23:43:55 +0800

    add 适配器demo

---
+++ src/main/java/shipeiqiMode/shpeiModel/Turkey.java
@@ -0,0 +1,12 @@
+package shipeiqiMode.shpeiModel;
+
+/**
+ * @Author:zhanCai
+ * @Description:
+ * @Date:Created in  23:38 2019/6/5
+ * @Modified by
+ */
+public interface Turkey {
+    public void gobble();
+    public void fly();
+}
